451e98c7541a1c90390758e28c38db12b6e17601
Mhezron <mhezron699@gmail.com>
2025-01-11 03:00:14 +0300

    updated the homepage

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,4 +1,5 @@
 class TeachersController < ApplicationController
+  before_action :set_teacher, only: [:edit, :update, :destroy, :show]
     def index
       @teachers = Teacher.all
     end
@@ -8,18 +9,39 @@ class TeachersController < ApplicationController
     def create
       @teacher = Teacher.new(teacher_params)
       if @teacher.save
-        redirect_to @teacher, notice: "Teacher was successfully added!"
+        redirect_to teachers_path, notice: "Teacher was successfully added!"
       else
-        render :new, status: :unporcessable_entity
+        render :new, status: :unprocessable_entity
       end
+    end
+
+    def edit
+    end
+
+    def update
+      if @teacher.update(teacher_params)
+        redirect_to teachers_path, notice: "Teacher was successfully updated!"
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      @teacher.destroy
+      redirect_to teacher_path, notice: "Teacher was successfully deleted!"
     end
     def show
       @teacher = Teacher.find(params[:id])
     end
 
     private
+    def set_teacher
+      @teacher = Teacher.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      redirect_to teachers_path, alert: "Teacher not found"
+    end
 
     def teacher_params
-      params.require(:teacher).permit(:teacher_fname, :teacher_lname, :mobile_no, :email, )
+      params.require(:teacher).permit(:teacher_fname, :teacher_lname, :mobile_no, :email, :subject_id, :dept_id)
     end
 end
